f66447dff37785f06e980b534948bc1205f35356
alpakaxaxa <34175210+alpakaxaxa@users.noreply.github.com>
2020-10-04 22:42:09 +0200

    Add files via upload

--- track.js
+++ track.js
@@ -1,17 +1,26 @@
 window.addEventListener("load", initWatchLocation)
+let play = document.getElementById("play");
+let pause = document.getElementById("pause");
+let mute = document.getElementById("mute");
+let progress = document.getElementById("progress");
+
+play.addEventListener("click", playAudio);
+pause.addEventListener("click", pauseAudio);
+mute.addEventListener("click", muteAudio);
+progress.addEventListener("click", seek);
 
 function initWatchLocation() {
     const APIRootURL = "http://localhost:1337";
     const URL = "http://localhost:1337/distinctive-places";
-    
+
     const locationMargin = 0.03;
     let audioTrack;
     let distinctivePlaces;
     let oldTargetArea;
     fetchDistinctivePlaces(URL)
-    .then((data) => {
-        distinctivePlaces = data;
-    })
+        .then((data) => {
+            distinctivePlaces = data;
+        })
     const options = {
         enableHighAccuracy: false,
         timeout: 5000,
@@ -62,10 +71,41 @@ function initWatchLocation() {
         let title = document.getElementById("title");
         let description = document.getElementById("description");
         let audio = document.getElementById("audio");
-        //let audio = new Audio(APIRootURL + distinctivePlace.AudioInformation.url)
         audio.src = APIRootURL + distinctivePlace.AudioInformation.url;
         title.innerHTML = distinctivePlace.Name;
         description.innerHTML = distinctivePlace.Description;
         oldTargetArea = distinctivePlace;
     }
+}
+
+function playAudio() {
+    let audio = document.getElementById("audio");
+    audio.play();
+    setInterval(function () {
+        updateSongProgress();
+    }, 1000);
+}
+
+function pauseAudio() {
+    let audio = document.getElementById("audio");
+    audio.pause();
+}
+
+function muteAudio() {
+    let audio = document.getElementById("audio");
+    audio.muted = !audio.muted;
+}
+
+function updateSongProgress() {
+    let audio = document.getElementById("audio");
+    let progress = document.getElementById("progress");
+    progress.value = Math.round(audio.currentTime / audio.duration * 100);
+}
+
+function seek(e) {
+    let audio = document.getElementById("audio");
+    let progress = document.getElementById("progress");
+    let percent = e.offsetX / this.offsetWidth;
+    audio.currentTime = percent * audio.duration;
+    progress.value = percent / 100;
 }
